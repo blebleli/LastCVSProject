eaadd8ed387700e87e3aec9d4d0bf8a71cd9f0dc
201802_JKH <201802_JKH@d808369f-3623-194b-b0f8-88541be8a525>
2018-09-03 03:45:43 +0000

    조계환

--- LastProject_CVS/src/main/java/kr/or/ddit/user/search/web/SearchController.java
+++ LastProject_CVS/src/main/java/kr/or/ddit/user/search/web/SearchController.java
@@ -1,12 +1,18 @@
 package kr.or.ddit.user.search.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RequestMapping("/search")
 @Controller("searchController")
 public class SearchController {
+	
+	private Logger logger = LoggerFactory.getLogger(SearchController.class);
 
 	@RequestMapping("/test")
 	 public String bView() {
@@ -25,5 +31,26 @@ public class SearchController {
 		
 		 return "prodSearch";
 	 }
+	
+	
+	/** 
+	 * Method   : cvsSearchAction 
+	 * 최초작성일  : 2018. 9. 3. 
+	 * 작성자 : 조계환 
+	 * 변경이력 :신규
+	 * @param model
+	 * @return 
+	 * Method 설명 : 
+	 */
+	@RequestMapping("/cvsSearchAction")
+	public String cvsSearchAction(@RequestParam(value="searchWord",defaultValue="word")String word, 
+								  Model model){
+		
+		System.out.println("==============================word : " + word);
+		logger.debug("==============================word : {}" , word);
+		
+		
+		return "redirect:/search/cvsSearch";
+	}
 	
 }
