c494c80c522185708b979201c7b663be7aaefa57
201802_HSJ <201802_HSJ@d808369f-3623-194b-b0f8-88541be8a525>
2018-09-14 07:02:38 +0000

    disp

---
+++ LastProject_CVS/src/main/java/kr/or/ddit/disposal/service/DispService.java
@@ -0,0 +1,51 @@
+package kr.or.ddit.disposal.service;
+
+import javax.annotation.Resource;
+
+import kr.or.ddit.disposal.dao.DispDaoInf;
+import kr.or.ddit.model.DisposalListVo;
+import kr.or.ddit.saleDisp.dao.SaleDisDaoInf;
+
+import org.springframework.stereotype.Service;
+
+
+/**
+ * 
+ * DispService.java 
+ * 
+ * @author 한수정
+ * @since 2018. 9. 14. 
+ * @version 1.0 
+ * @see pos 에서의 판매, 폐기
+ * 
+ * <pre> 
+ * << 개정이력(Modification Information) >> 
+ *   
+ *   수정일  수정자 수정내용 
+ * ---------- ------ ------------------------
+ * 2018. 9. 14.    PC06 최초 생성 
+ * 
+ * </pre>
+ */
+@Service("dispService")
+public class DispService implements DispServiceInf{
+
+	@Resource(name="dispDao")
+	private DispDaoInf dispDao;
+	
+	/**
+	 * 
+	 * Method   : insertDispList 
+	 * 최초작성일  : 2018. 9. 14. 
+	 * 작성자 : 한수정
+	 * 변경이력 : 
+	 * @param disposalListVo
+	 * @return 
+	 * Method 설명 : disposalList에 insert 하는 기능
+	 */
+	@Override
+	public int setInsertDispList(DisposalListVo disposalListVo) {
+		return dispDao.setInsertDispList(disposalListVo);		
+	}
+
+}
